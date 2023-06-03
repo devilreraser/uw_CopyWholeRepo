43b244ef7d422739b103017f65505a44e91724da
Dimitar Lilov <devilerser@gmail.com>
2023-06-03 22:44:32 +0300

    ... still working on main repo clone

--- 05_Project/repo_clone.bat
+++ 05_Project/repo_clone.bat
@@ -3,14 +3,15 @@ setlocal
 
 :: Example usage:
 ::
-:: clone_repo.bat https://github.com/example/source-repo.git https://github.com/example/destination-repo.git <github_personal_access_token>
+:: repo_clone.bat https://github.com/your-username/source-repo.git https://github.com/your-username/destination-repo.git your_github_token C:\path\to\temp\directory
 :: ex.:
-:: repo_clone.bat https://github.com/devilreraser/uw_repository_utils.git https://github.com/devilreraser/uw_repository_utils_cloned.git ghp_OTAbETGQXAXUJy5Jgb958wrcRfbwtq2Ltv9w
+:: repo_clone.bat https://github.com/devilreraser/fw_nRF_SPI_BLE.git https://github.com/devilreraser/fw_nRF_SPI_BLE_cloned.git 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 ..\..
 ::
-:: Replace "https://github.com/example/source-repo.git" with the actual URL of the source repository.
-:: Replace "https://github.com/example/destination-repo.git" with the actual URL of the destination repository.
-:: Replace <github_personal_access_token> with your actual GitHub personal access token ex: ghp_OTAbETGQXAXUJy5Jgb958wrcRfbwtq2Ltv9w.
-:: If the token is not provided as a command-line argument, you will be prompted to enter it interactively.
+:: Replace "https://github.com/your-username/source-repo.git" with the actual URL of the source repository.
+:: Replace "https://github.com/your-username/destination-repo.git" with the actual URL of the destination repository.
+:: Replace "your_github_token" with your actual GitHub personal access token, e.g., ghp_OTAbETGQXAXUJy5Jgb958wrcRfbwtq2Ltv9w.
+:: Replace "C:\path\to\temp\directory" with the actual path where the temporary directory should be created.
+:: If any of the parameters are not provided as command-line arguments, you will be prompted to enter them interactively.
 
 :: Check if source repository URL is provided as a command-line argument
 if "%~1"=="" (
@@ -33,32 +34,45 @@ if "%~3"=="" (
     set "github_token=%~3"
 )
 
-:: Clone the source repository
-git clone --recursive %source_repo% temp_repo
-cd temp_repo
+:: Check if temporary directory path is provided as a command-line argument
+if "%~4"=="" (
+    set /p temp_dir=Enter the temporary directory path:
+) else (
+    set "temp_dir=%~4"
+)
 
 :: Get the repository name from the source URL
-for %%i in ("%source_repo%") do set "repo_name=%%~ni"
+for %%i in ("%source_repo%") do set "source_repo_name=%%~ni"
+
+:: Check if the source repository is already cloned
+if exist "%temp_dir%\%source_repo_name%" (
+    echo Source repository already cloned.
+
+    :: Change to the source repository directory
+    cd "%temp_dir%\%source_repo_name%"
+
+    :: Check if the cloned repository has uncommitted changes
+    git diff-index --quiet HEAD --
+    if not errorlevel 1 (
+        echo The cloned repository has uncommitted changes. Aborting.
+        exit /b 1
+    )
+) else (
+    :: Create the temporary directory
+    mkdir "%temp_dir%"
+
+    :: Clone the source repository
+    echo Cloning the source repository...
+    git clone --recursive "%source_repo%" "%temp_dir%\%source_repo_name%"
+
+    :: Change to the source repository directory
+    cd "%temp_dir%\%source_repo_name%"
+)
+
+:: Get the repository name from the destination URL
+for %%i in ("%destination_repo%") do set "destination_repo_name=%%~ni"
 
 :: Create a new repository on the destination remote using GitHub API
-echo Creating new repository: %repo_name%
+echo Creating new repository: %destination_repo_name%
 echo.
-curl -X POST -H "Authorization: token %github_token%" -d "{\"name\":\"%repo_name%\"}" "https://api.github.com/user/repos"
-
-:: Push the repository to the destination remote
-echo Pushing repository to destination...
-git remote set-url origin %destination_repo%
-git push --all
-
-:: Update the submodules
-echo Updating submodules...
-git submodule foreach --recursive git checkout master
-git submodule foreach --recursive git pull
-
-:: Clean up
-cd ..
-rmdir /s /q temp_repo
-
-echo.
-echo Repository and submodules cloned and pushed to the destination remote successfully!
-endlocal
+curl -X POST -H "Authorization: token %github_token%" -d "{\"name\":\"
